168d61ef568a4ded43dfa569a131ff30de9b6e0d
digvijaybhonsle <digvijaybhonsle007@gmail.com>
2025-01-03 14:40:43 +0530

    adding main page

---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,18 @@
+
+interface PropType {
+    placeholder: string;
+    size: "big" | "small",
+}
+
+export function TextInput({
+    placeholder,
+    size,
+}: PropType) {
+    return <input placeholder={placeholder} style={{
+        padding: size === "big" ? 20 : 10,
+        border: '1px solid #ccc',
+        borderRadius: 10,
+        margin: size === "big" ? 20 : 10,
+    }}>
+    </input>
+}
